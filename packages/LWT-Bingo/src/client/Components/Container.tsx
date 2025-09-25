58b68f03ab5da5791bcdafbf108c4ea9ed60ca43
Lane Hamilton <aleyna.hamilton@gmail.com>
2025-09-25 13:36:29 -0700

    leaderboard position below board

--- packages/LWT-Bingo/src/client/Components/Container.tsx
+++ packages/LWT-Bingo/src/client/Components/Container.tsx
@@ -45,8 +45,11 @@ export default function Container() {
         display="flex"
         gap={2}
         justifyContent={'center'}
-        alignItems={isMobile || isLandscapeMobile ? 'center' : 'flex-start'}
-        flexDirection={isMobile || isLandscapeMobile ? 'column' : 'row'}
+        /* UNCOMMENT FOR LEADERBOARD NEXT TO BOARD */
+        // alignItems={isMobile || isLandscapeMobile ? 'center' : 'flex-start'}
+        // flexDirection={isMobile || isLandscapeMobile ? 'column' : 'row'}
+        alignItems={'center'} // leaderboard below board all screen sizes
+        flexDirection={'column'} // leaderboard below board all screen sizes
       >
         <Board />
         <Leaderboard />
